6d8f7ec15cff857631695e4cd209586676f98178
kanakodavis <matthiasgusenbauer@gmx.net>
2012-12-15 11:58:09 +0100

    Added raw version of the bakery.

---
+++ Bakery.java
@@ -0,0 +1,16 @@
+
+public class Bakery {
+	
+	public Bakery() {
+		
+		//TODO: create all machines
+		
+	}
+	
+	public void /*KEKSDOSE*/ processOrder(/*Order order*/) {
+		
+		//TODO: will process the order
+		
+	}
+
+}
