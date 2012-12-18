832e9363ad652a8499ce053e34eb221a9ed88288
kanakodavis <matthiasgusenbauer@gmx.net>
2012-12-18 22:08:20 +0100

    NOW preasseeee

--- Bakery.java
+++ Bakery.java
@@ -8,6 +8,10 @@ public class Bakery {
 	
 	private final DoppelKeksiMaschine doubleMachine;
 	
+	/**
+	 * Constructor instantiates the doubleCookieMachine
+	 * 
+	 */
 	//Postcondition: doubleMachine is instantiated with doubleMachine = new DoppelKeksiMaschine()
 	public Bakery() {
 		
@@ -15,6 +19,12 @@ public class Bakery {
 
 	}
 	
+	/**
+	 * This method takes an Bestellung object, processes the order and returns a delicious smelling CookieJar
+	 * 
+	 * @param order	A collection of Position objects which contain the details of the order
+	 * @return	A cookieJar containing fresh baked delicious Kekse.
+	 */
 	//Precondition: order != null
 	//Postcondition: returns a Keksdose containing all Kekse according to order != null;
 	public Keksdose processOrder(Bestellung order) {
@@ -29,7 +39,7 @@ public class Bakery {
 			
 			for(int i = 0; i < cookieCount; i++) {
 				
-				if(p.getFilling() != null){
+				if(p.getFilling() == null){
 					insertList.add(p.getShape().bakeWith().bake(p.getDough()));
 					
 				} else {
